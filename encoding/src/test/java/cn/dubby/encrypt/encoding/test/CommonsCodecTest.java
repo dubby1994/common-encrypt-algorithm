fd22718ca5203669df808f354e68559dc0a4610c
daoxuan <daoxuan@pinduoduo.com>
2018-11-10 22:44:09 -0800

    base32

--- encoding/src/test/java/cn/dubby/encrypt/encoding/test/CommonsCodecTest.java
+++ encoding/src/test/java/cn/dubby/encrypt/encoding/test/CommonsCodecTest.java
@@ -1,5 +1,6 @@
 package cn.dubby.encrypt.encoding.test;
 
+import org.apache.commons.codec.binary.Base32;
 import org.apache.commons.codec.binary.Base64;
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,8 +16,8 @@ public class CommonsCodecTest {
     private static final String originStr = "Hello, world.你好，世界。" + System.currentTimeMillis();
 
     @Test
-    public void encodeDecode() {
-        System.out.println("encodeDecode");
+    public void encodeDecodeBase64() {
+        System.out.println("encodeDecodeBase64");
         byte[] srcBytes = originStr.getBytes(Charset.forName("UTF-8"));
         byte[] encodeBytes = Base64.encodeBase64(srcBytes);
         String encodeStr = new String(encodeBytes, Charset.forName("UTF-8"));
@@ -30,14 +31,30 @@ public class CommonsCodecTest {
     }
 
     @Test
-    public void encodeDecodeURLSafe() {
-        System.out.println("encodeDecodeURLSafe");
+    public void encodeDecodeURLSafeBase64() {
+        System.out.println("encodeDecodeURLSafeBase64");
         byte[] srcBytes = originStr.getBytes(Charset.forName("UTF-8"));
         byte[] encodeBytes = Base64.encodeBase64URLSafe(srcBytes);
         String encodeStr = new String(encodeBytes, Charset.forName("UTF-8"));
         System.out.println(encodeStr);
 
         byte[] decodeBytes = Base64.decodeBase64(encodeBytes);
+        String decodeStr = new String(decodeBytes, Charset.forName("UTF-8"));
+        System.out.println(decodeStr);
+
+        Assert.assertEquals(originStr,  decodeStr);
+    }
+
+    @Test
+    public void encodeDecodeBase32() {
+        System.out.println("encodeDecodeBase32");
+        byte[] srcBytes = originStr.getBytes(Charset.forName("UTF-8"));
+        Base32 base32 = new Base32();
+        byte[] encodeBytes = base32.encode(srcBytes);
+        String encodeStr = new String(encodeBytes, Charset.forName("UTF-8"));
+        System.out.println(encodeStr);
+
+        byte[] decodeBytes = base32.decode(encodeBytes);
         String decodeStr = new String(decodeBytes, Charset.forName("UTF-8"));
         System.out.println(decodeStr);
 
