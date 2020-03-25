7bfa6d0a675cd9cc6d532dd515736c994dbf9d48
daoxuan <daoxuan@pinduoduo.com>
2020-03-25 20:44:28 +0800

    test hex

--- encoding/src/test/java/cn/dubby/encrypt/encoding/test/HexUtilTest.java
+++ encoding/src/test/java/cn/dubby/encrypt/encoding/test/HexUtilTest.java
@@ -5,6 +5,8 @@ import org.apache.commons.codec.binary.Hex;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.nio.charset.StandardCharsets;
+
 public class HexUtilTest {
 
     @Test
@@ -35,6 +37,34 @@ public class HexUtilTest {
         for (int i = 0; i < 9; ++i) {
             System.out.println(i + "\t" + (1 << i - 1));
         }
+    }
+
+    @Test
+    public void testMatch() {
+        String str = "我是道玄，我在测试，abc哈哈哈";
+        String subStr1 = "道玄";
+        String subStr2 = "a";
+        String subStr3 = "道";
+        String subStr4 = "玄";
+
+        String hex = HexUtil.toHex(str.getBytes(StandardCharsets.UTF_8));
+        System.out.println(hex);
+
+        String subHex1 = HexUtil.toHex(subStr1.getBytes(StandardCharsets.UTF_8));
+        System.out.println(subHex1);
+        System.out.println(hex.indexOf(subHex1));
+
+        String subHex2 = HexUtil.toHex(subStr2.getBytes(StandardCharsets.UTF_8));
+        System.out.println(subHex2);
+        System.out.println(hex.indexOf(subHex2));
+
+        String subHex3 = HexUtil.toHex(subStr3.getBytes(StandardCharsets.UTF_8));
+        System.out.println(subHex3);
+        System.out.println(hex.indexOf(subHex3));
+
+        String subHex4 = HexUtil.toHex(subStr4.getBytes(StandardCharsets.UTF_8));
+        System.out.println(subHex4);
+        System.out.println(hex.indexOf(subHex4));
     }
 
 }
